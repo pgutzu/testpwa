88faf5a8fa941bea6e005c9df8d773e978de9acd
Pavel <pgutzu@gmail.com>
2020-02-07 22:36:21 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Button, message } from 'antd';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import 'antd/dist/antd.css';
+class App extends React.Component {
+  fakeFetch = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => message.info(JSON.stringify(json)))
+      .catch(e => {
+        message.info(JSON.stringify(e))
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Button
+            onClick={() => this.fakeFetch()}
+          >
+            Send Message
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

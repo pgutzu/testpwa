a3ce565317911b77b77d2bf2e89596126e2f5827
Pavel <pgutzu@gmail.com>
2020-02-07 22:04:29 +0300

    second commit

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -53,7 +53,7 @@ export function register(config) {
 
       console.log("load")
 
-      const swUrl = `https://testpwareact.herokuapp.com//service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       console.log("swUrl", swUrl)
 
 
